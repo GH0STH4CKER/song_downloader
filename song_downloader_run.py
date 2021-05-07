98b805e77d4bb9f48408348b7ec634bd7b1b1be2
Dimuth De Zoysa <dimuthsakya@protonmail.com>
2021-05-07 16:32:30 +0530

    Create song_downloader_run.py

---
+++ song_downloader_run.py
@@ -0,0 +1,14 @@
+from colorama import Fore , init
+
+init()
+banner = """
+  █▀ █▀█ █▄ █ █▀▀   █▀▄ █▀█ █ █ █ █▄ █ █   █▀█ ▄▀█ █▀▄
+  ▄█ █▄█ █ ▀█ █▄█   █▄▀ █▄█ ▀▄▀▄▀ █ ▀█ █▄▄ █▄█ █▀█ █▄▀
+--------------------------------------------------------"""
+tag = """     [+] Made By GH0STH4CK3R       [+] Version 1.0
+--------------------------------------------------------"""
+
+print(Fore.LIGHTGREEN_EX + banner)
+print(Fore.LIGHTYELLOW_EX + tag)
+
+import song_downloader
